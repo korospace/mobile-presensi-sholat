0df3d0741aa2f2b7ec49549e762b6f02a30db50c
korospace <bagaselkoro@gmail.com>
2022-12-13 13:58:40 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobile-presensi-sholat',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
